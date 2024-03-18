d9c050ffd3f32f04fefc45b6f9e8a1ac5c8c0e75
Thanhnhanle123 <nestalect65@gmail.com>
2024-03-19 00:03:11 +0700

    add

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="page">
+        <h4>Thêm liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+    </div>
+</template>
+
+<script>
+import ContactForm from '@/components/ContactForm.vue';
+import ContactService from '@/services/contact.service';
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {
+                name: '',
+                email: '',
+                address: '',
+                phone: '',
+                favorite: false,
+            },
+            message: '',
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                console.log(data);
+                await ContactService.create(data);
+                alert("Liên hệ được thêm thành công");   //tao thông báo đã thêm thành công
+                this.$router.push({ name: 'contactbook' });
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    },
+    created() {
+        this.message = '';
+    },
+};
+</script>
